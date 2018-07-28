4b17059d9524d31dbc4d042c4440d5214468422e
Morrowii <sadler.m.ryan@gmail.com>
2018-07-28 13:19:59 -0400

    cleaned up schema

--- models/schema.sql
+++ models/schema.sql
@@ -2,37 +2,3 @@
 CREATE DATABASE IF NOT EXISTS bookieDB;
 
 USE bookieDB;
-
-create table matches (
-    match_id int not null auto_increment,
-    primary key (match_id),
-	match_name varchar(255) not null,
-    match_start varchar(255) not null, 
-    match_end varchar(255) not null, 
-    team_A varchar(255) not null,
-    team_B varchar(255) not null,
-    match_odds float not null default 0.5,
-    match_result boolean not null
-)
-
-create table accounts (
-    account_id int not null auto_increment,
-    primary key (account_id),
-	account_name varchar(255) not null,
-    account_points int not null,
-    wagers_current varchar(255) not null,
-    wagers_history varchar(255) not null
-)
-
-create table admin_current (
-    match_id int not null,
-    primary key (match_id),
-    match_wager int not null
-)
-
-create table admin_history (
-    match_id int not null,
-    primary key (match_id),
-    match_wager int not null,
-    wager_returns int not null
-)
