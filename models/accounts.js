26dc266b2ec08377ea722bad32140bc4c0e2219c
Morrowii <sadler.m.ryan@gmail.com>
2018-07-30 19:02:26 -0400

    edited models

--- models/accounts.js
+++ models/accounts.js
@@ -1,5 +1,11 @@
 module.exports = function(sequelize, DataTypes) {
   var Accounts = sequelize.define("Accounts", {
+    account_id: {
+      type: DataTypes.INT,
+      allowNull: false,
+      primaryKey: true,
+      autoincrement: true
+    },
     account_name: {
       type: DataTypes.STRING,
       allowNull: false
--- models/matches.js
+++ models/matches.js
@@ -3,18 +3,13 @@ module.exports = function(sequelize, DataTypes) {
     match_id: {
       type: DataTypes.INT,
       allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
+      primaryKey: true
     },
     match_name: {
       type: DataType.STRING,
       allowNull: false
     },
     match_start: {
-      type: DataTypes.DATE,
-      allowNull: false
-    },
-    match_end: {
       type: DataTypes.DATE,
       allowNull: false
     },
