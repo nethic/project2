24452d3c8e1ef43e538a09beb4424cdf475529fd
Morrowii <sadler.m.ryan@gmail.com>
2018-08-07 23:03:42 -0400

    cleaned up package-lock.json again

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var PORT = process.env.PORT || 3007;
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static("public"));
+app.use(express.static("./assets"));
 
 // Handlebars
 /*
