0aefe7b5b7d745ebcba95cae5e098a376436f3b4
Morrowii <sadler.m.ryan@gmail.com>
2018-08-08 20:37:57 -0400

    exposed front-end design on alt route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -8,6 +8,10 @@ module.exports = function(app) {
     res.sendFile(path.resolve('./views/indexAlt.html'));
   });
 
+  app.get('/proper', function (req, res) {
+    res.sendFile(path.resolve('./views/index.html'));
+  });
+
 //   // Load example page and pass in an example by id
 //   app.get("/example/:id", function(req, res) {
 //     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
